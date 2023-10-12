f5c9561a37fbfce64c5c6f35e021237cb283eec6
Wladson Ramos <wladsonramossi@gmail.com>
2023-10-12 17:57:13 -0300

    implementing security on user data

--- src/main/java/br/com/wladsonramos/todolist/user/UserController.java
+++ src/main/java/br/com/wladsonramos/todolist/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,10 @@ public class UserController {
         if(user != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passswordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passswordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
