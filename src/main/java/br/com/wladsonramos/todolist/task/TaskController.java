3a7f09acd7511ce904075d20269ad36f9d6a5ef2
Wladson Ramos <wladsonramossi@gmail.com>
2023-10-14 14:55:44 -0300

    validating users and tasks

--- src/main/java/br/com/wladsonramos/todolist/task/TaskController.java
+++ src/main/java/br/com/wladsonramos/todolist/task/TaskController.java
@@ -53,10 +53,22 @@ public class TaskController {
     }
  
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
         var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+        if(!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
-        return this.taskRepository.save(task);
+        
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
